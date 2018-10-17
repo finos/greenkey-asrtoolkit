0869ca482e546f06f05ee0d52bb80be8746de24b
Matthew Goldey <matthew.goldey@gmail.com>
2018-10-17 16:02:45 -0500

    generalize audio corpus prep (#15)
    This PR enables audio corpus objects to accept SPH, WAV, and MP3 files from directories. It still expects file names to match between audio files and transcript STM files.
    
    Further, this PR mixes stereo channels down to mono instead of discarding extra channels.
    
    Fixes #13

--- asrtoolkit/file_utils/name_cleaners.py
+++ asrtoolkit/file_utils/name_cleaners.py
@@ -11,6 +11,13 @@ def basename(file_name):
   return file_name.split("/")[-1]
 
 
+def strip_extension(file_name):
+  """
+    Reutrns file without extension
+  """
+  return ".".join(file_name.split(".")[:-1]) if file_name else ""
+
+
 def sanitize_hyphens(file_name):
   """
     Replace hyphens with underscores if present in file name
@@ -20,7 +27,7 @@ def sanitize_hyphens(file_name):
       "Replacing hyphens with underscores in SPH file output - "
       "check to make sure your audio files and transcript files match"
     )
-    file_name = "/".join(file_name.split("/")[:-1] + [file_name.split("/")[-1].replace("-", "_")])
+    file_name = "/".join(file_name.split("/")[:-1] + [basename(file_name).replace("-", "_")])
   return file_name
 
 
@@ -28,5 +35,5 @@ def generate_segmented_file_name(target_dir, file_name, iseg):
   """
     Take a target location, a current location, and a segment number and generate a target filename
   """
-  return target_dir + "/" + ".".join(file_name.split("/")[-1].split(".")[:-1]) + \
+  return target_dir + "/" + basename(strip_extension(file_name)) + \
             "_seg_{:05d}.".format(iseg) + file_name.split(".")[-1]
