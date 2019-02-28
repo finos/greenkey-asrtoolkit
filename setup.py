90379e9e4e8b45ce6dbc70fa1b711e5b38b604e3
Matthew Goldey <matthew.goldey@gmail.com>
2019-02-28 14:16:56 -0600

    release 0.1.13

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
   name='asrtoolkit',
-  version='0.1.12',
+  version='0.1.13',
   description=
   'The GreenKey ASRToolkit provides tools for automatic speech recognition (ASR) file conversion and corpora organization.',
   long_description=long_description,
