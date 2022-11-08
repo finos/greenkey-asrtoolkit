a72ab9071503d655198072377235ef836fd875c8
Matthew Goldey <matthew.goldey@gmail.com>
2022-11-08 11:36:02 -0500

    enable the target speaker word diarization error stat calculation to calculate fbeta and include insertions and deletions

--- asrtoolkit/metrics/tswde.py
+++ asrtoolkit/metrics/tswde.py
@@ -14,13 +14,16 @@ from asrtoolkit.file_utils.script_input_validation import assign_if_valid
 from .wder import get_words_and_index_mapping
 
 
-def tswde(ref, hyp, target_speaker):
+def tswde(
+    ref, hyp, target_speaker, beta=1.0, include_deletions=True, include_insertions=True
+):
     """
     Computes target speaker word diarization error statistics
 
     Inspired by the word diarization error rate
 
-    Returns f1-score, precision, and recall for correct ASR words and substitutions based on the speaker identification
+    Returns f-score, precision, and recall for the assignment of words to speakers
+    for a given target speaker as compared to all other speakers
     """
 
     ref_words, ref_word_idx_dict = get_words_and_index_mapping(ref)
@@ -44,22 +47,43 @@ def tswde(ref, hyp, target_speaker):
                 hyp_speaker = speaker_mapping[
                     hyp.segments[hyp_word_idx_dict[hyp_idx]].speaker
                 ]
+
+                # words which belong to the officer are attributed to the officer
                 true_positives += ref_speaker == hyp_speaker == target_speaker
+
+                # words which belong to non-officer are attributed to the officer
                 false_positives += (
                     hyp_speaker == target_speaker and ref_speaker == other
                 )
+
+                # words which belong to non-officer are attributed to the non-officer
                 true_negatives += ref_speaker == hyp_speaker == other
+
+                # words which belong to officer are attributed to the non-officer
                 false_negatives += (
                     ref_speaker == target_speaker and hyp_speaker == other
                 )
-        # note that we ignore all insertions and deletions since it's ambiguous
-        # to which speaker these words belong
+        elif operation.tag == "delete" and include_deletions:
+            for idx in range(operation.src_start, operation.src_end):
+                ref_speaker = speaker_mapping[
+                    ref.segments[ref_word_idx_dict[idx]].speaker
+                ]
+                # words which belong to officer are attributed to the non-officer
+                false_negatives += ref_speaker == target_speaker
+
+        elif operation.tag == "insert" and include_insertions:
+            for idx in range(operation.dest_start, operation.dest_end):
+                hyp_speaker = speaker_mapping[
+                    hyp.segments[hyp_word_idx_dict[idx]].speaker
+                ]
+                # words which belong to non-officer are attributed to the officer
+                false_positives += hyp_speaker == target_speaker
 
     precision = true_positives / (true_positives + false_positives)
     recall = true_positives / (true_positives + false_negatives)
-    f1 = 2 * (precision * recall) / (precision + recall)
+    fscore = (1 + beta**2) * (precision * recall) / ((beta**2 * precision) + recall)
 
-    return {"f1": f1, "precision": precision, "recall": recall}
+    return {"fscore": fscore, "precision": precision, "recall": recall}
 
 
 def compute_tswde(reference_file, transcript_file, target_speaker, json_format=None):
